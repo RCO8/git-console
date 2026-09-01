0d9cafc7306e8361a75cd999b8081fcdc4f9d23f
RCO8 <gurwo63@naver.com>
2026-09-01 12:38:11 +0900

    feat: print student score

--- src/main/java/com/sparta/Main.java
+++ src/main/java/com/sparta/Main.java
@@ -2,6 +2,8 @@ package com.sparta;
 
 public class Main {
     public static void main(String[] args) {
-
+        String name = "김하나";
+        int score = 87;
+        System.out.println(name + " 학생의 점수는 " + score + "점입니다.");
     }
 }
